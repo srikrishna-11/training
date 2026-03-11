277a26c442351e97a7ac2402e2171bf8cc4e4334
srikrishna-11 <SriKrishna.R@cognizant.com>
2026-03-11 18:15:54 +0530

    Update wheat.js

--- blocks/wheat/wheat.js
+++ blocks/wheat/wheat.js
@@ -2,4 +2,10 @@ export default function decorate (block) {
 const first = block.children[0]; if (!first) return;
 first.classList.add('wheat-strategy');
 
+  const second = block.children[1]; if (!second) return;
+second.classList.add('wheat-grant');
+
+  const third = block.children[2]; if (!third) return;
+third.classList.add('wheat-management');
+
 }
