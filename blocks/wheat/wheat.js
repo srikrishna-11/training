4b201faca4adf73fbfc6bcbb5e696678d8d1aa30
srikrishna-11 <SriKrishna.R@cognizant.com>
2026-03-11 18:17:36 +0530

    Update wheat.js

--- blocks/wheat/wheat.js
+++ blocks/wheat/wheat.js
@@ -8,4 +8,14 @@ second.classList.add('wheat-grant');
   const third = block.children[2]; if (!third) return;
 third.classList.add('wheat-management');
 
+  const four = block.children[3]; if (!four) return;
+four.classList.add('wheat-strategy');
+
+  const five = block.children[4]; if (!five) return;
+five.classList.add('wheat-grant');
+
+  const six = block.children[5]; if (!six) return;
+six.classList.add('wheat-management');
+  
+
 }
