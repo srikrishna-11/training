f815698b5082b5f7a824743e1749a745b192229f
srikrishna-11 <SriKrishna.R@cognizant.com>
2026-03-11 17:36:44 +0530

    Update wheat.js

--- blocks/wheat/wheat.js
+++ blocks/wheat/wheat.js
@@ -1,33 +1,38 @@
-<script>
-  (function () {
-    // Map card keys to routes or custom actions
-    const routes = {
-      'strategy':        () => window.location.href = '#strategy',
-      'grant-writing':   () => window.location.href = '#grant-writing',
-      'management':      () => window.location.href = '#management',
-      'teaching':        () => window.location.href = '#teaching',
-      'campaign':        () => window.location.href = '#campaign',
-      '247-support':     () => window.location.href = '#support'
-    };
-
-    // Delegate clicks
-    document.addEventListener('click', (e) => {
-      const card = e.target.closest('.svc-card');
-      if (!card) return;
-      const key = card.dataset.key;
-      if (routes[key]) routeskey;
-      else console.log('Card clicked:', key);
+export default function decorate(block) {
+    const newsComponent = block.children[0];
+    newsComponent.classList.add('news_component');
+    const component = block.children[0].children[0];
+    component.classList.add('component');
+ 
+    const newsHeader = block.querySelector('.component h2');
+    const newsDesc = block.querySelector('.component p');
+    if (newsHeader) {
+        newsHeader.classList.add('news_header');
+    }
+    if (newsDesc) {
+        newsDesc.classList.add('news_desc');
+    }
+    const newsContainer = block.children[1];
+    if (newsContainer) {
+        newsContainer.classList.add('news_items');
+    }
+    const news = block.querySelectorAll('.news_items > div');
+    news.forEach((element) => {
+        element.classList.add('news_methods');
     });
-
-    // Keyboard: Enter/Space
-    document.addEventListener('keydown', (e) => {
-      if (e.key !== 'Enter' && e.key !== ' ') return;
-      const card = e.target.closest('.svc-card');
-      if (!card) return;
-      e.preventDefault();
-      const key = card.dataset.key;
-      if (routes[key]) routeskey;
-      else console.log('Card activated via keyboard:', key);
+    const newsMethods = block.querySelectorAll('.news_methods > h3');
+    newsMethods.forEach((element) => {
+        element.classList.add('news_title');
+    });   
+    const newsPara = block.querySelectorAll('.news_methods > p');
+    newsPara.forEach((element) => {
+        element.classList.add('news_para');
     });
-  })();
-</script>
+    const newsInfo = block.querySelectorAll('.news_methods > h4');
+    newsInfo.forEach((element) => {
+        element.classList.add('news_info');
+    });
+    const newsRead = block.querySelectorAll('.news_methods > h5');
+    newsRead.forEach((element) => {
+        element.classList.add('news_read');
+    });
