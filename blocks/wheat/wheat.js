60efa5545632816da0cb730768727049a429aea1
srikrishna-11 <SriKrishna.R@cognizant.com>
2026-03-11 17:58:13 +0530

    Update wheat.js

--- blocks/wheat/wheat.js
+++ blocks/wheat/wheat.js
@@ -3,13 +3,5 @@ const first = block.children[0]; if (!first) return;
 first.classList.add('wheat-strategy');
 
   const first = block.querySelector('.wheat-strategy p');
-    const first = block.querySelector('.wheat-strategy p');
-    if (newsHeader) {
-        newsHeader.classList.add('news_header');
-    }
-    if (newsDesc) {
-        newsDesc.classList.add('news_desc');
-    }
-
-  
+    
 }
