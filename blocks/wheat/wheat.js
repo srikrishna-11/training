0e1a063ed6de9d6228fd08c5f3b28ce292029a17
srikrishna-11 <SriKrishna.R@cognizant.com>
2026-03-11 17:46:31 +0530

    Update wheat.js

--- blocks/wheat/wheat.js
+++ blocks/wheat/wheat.js
@@ -1,15 +1,6 @@
-export default function decorate(block) {
-    const newsComponent = block.children[0];
-    newsComponent.classList.add('news_component');
-    const component = block.children[0].children[0];
-    component.classList.add('component');
+export default function decorate (block) {
  
-    const newsHeader = block.querySelector('.component h2');
-    const newsDesc = block.querySelector('.component p');
-    if (newsHeader) {
-        newsHeader.classList.add('news_header');
-    }
-    if (newsDesc) {
-        newsDesc.classList.add('news_desc');
-    }
-    
+const first = block.children[0]; if (!first) return;
+ 
+first.classList.add('nav-head');
+}
